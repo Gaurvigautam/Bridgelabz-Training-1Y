4826f6a04fcea913583b7c38f6eda63f5419b4e4
Gaurvi Gautam <panditgauri@gmail.com>
2026-01-12 13:09:53 +0530

    Find power of any no.

---
+++ basic-programs/PowerCalculation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class PowerCalculation{
+  public static void main(String[] args){
+  Scanner sc =  new Scanner(System.in);
+  int base= sc.nextInt();
+  int exponent= sc.nextInt();
+  double Power = base * exponent;
+  System.out.println(Power);
+  }
+}
