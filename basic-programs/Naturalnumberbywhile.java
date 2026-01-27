075e34bd1606a91d3085daea655549447bae08d8
Gaurvigautam <panditgauri259@gmail.com>
2026-01-27 21:09:09 +0530

    Naturalnumberbywhile

---
+++ basic-programs/Naturalnumberbywhile.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class SumofNaturalno{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the number:");
+		int number = sc.nextInt();
+	
+		if (number>0){
+			int sumloop = 0;
+			int i = 1;
+			while (i <= n){
+			Sumloop = Sumloop + i;
+			i++;
+			}
+			int sum = number*(number+1)/2;
+			System.out.println("Naturalnumber");
+			System.out.println("not a Naturalnumber");
+			
+			 if (SumLoop == SumofNaturalno) {
+                System.out.println("Both computations are correct and equal.");
+            } else {
+                System.out.println("The computations are not equal.");
+            }
+
+        } else {
+            System.out.println("The number is not a natural number.");
+        }
+
+        sc.close();
+    }
+}
+
+			
+			
