d04d0c62fb857591bf3bb95282c2a4d215d445c5
Gaurvigautam <panditgauri259@gmail.com>
2026-01-27 21:20:27 +0530

    Employeesalary

---
+++ basic-programs/Employeesalary.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Employeesalary{
+    public static void main(String [] args){
+	  Scanner sc = new Scanner(System.in);
+	  
+	  System.out.println("Enter employee salary");
+	  double salary = sc.nextDouble();
+	  
+	  System.out.println("Enter the service years");
+	  double years = sc.nextDouble();
+	  
+	  if(years>5){
+		  double bonus = salary *0.05;
+				System.out.println("amount of Bonus"+bonus);  
+		  }else {
+			  System.out.println("no bonus year of service is less than or equal 5. ");  
+		  }
+		  }
+}
