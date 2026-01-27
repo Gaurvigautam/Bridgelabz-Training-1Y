5aa80ec7d5703460ea0478b9b7000d385cf0ce45
Gaurvigautam <panditgauri259@gmail.com>
2026-01-27 21:31:13 +0530

    Powerbywhile

---
+++ basic-programs/Powerbywhile.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PowerbyWhile {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Enter the power: ");
+        int power = sc.nextInt();
+
+        // Check for positive integers
+        if (number > 0 && power >= 0) {
+
+            int result = 1;
+            int counter = 0;
+
+            // while loop
+            while (counter < power) {
+                result = result * number;
+                counter++;
+            }
+
+            System.out.println(number + " raised to the power " + power + " is: " + result);
+
+        } else {
+            System.out.println("Please enter positive integers only.");
+        }
+
+        sc.close();
+    }
+}
